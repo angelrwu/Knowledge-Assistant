1480fba686c84715c52761558da6983bacb7f1cd
Angel Wu <44008675+arawup@users.noreply.github.com>
2019-11-12 03:01:06 -0500

    Initial commit

---
+++ Model/VocabularySets.js
@@ -0,0 +1,19 @@
+const vocabularySet2 = [
+    {
+        keyword:"A",
+        definition:"is for Alpha"
+    },
+    {
+        keyword:"B",
+        definition:"is for Beta"
+    },
+    {
+        keyword:"C",
+        definition:"is for Charley"
+    },
+    {
+        keyword:"D",
+        definition:"is for Delta"
+    }
+];
+
---
+++ Model/jsonFolders.js
@@ -0,0 +1,65 @@
+const jsonFile = [
+    {
+        fileName:"alphabetCodenames",
+        vocabularySet:[
+            {
+                keyword:"A",
+                definition:"is for Alpha"
+            },
+            {
+                keyword:"B",
+                definition:"is for Beta"
+            },
+            {
+                keyword:"C",
+                definition:"is for Charley"
+            },
+            {
+                keyword:"D",
+                definition:"is for Delta"
+            }
+        ]
+    },
+    {
+        fileName:"random",
+        vocabularySet:[
+            {
+                keyword:"Pi",
+                definition:"is 3.14"
+            },
+            {
+                keyword:"Tao",
+                definition:"is 6.28"
+            },
+            {
+                keyword:"Astrology",
+                definition:"the believe that the stars can determine one's life properties or story"
+            },
+            {
+                keyword:"Astronomy",
+                definition:"is science that study the stars"
+            }
+        ]
+    },
+    {
+        fileName:"test42",
+        vocabularySet:[
+            {
+                keyword:"Pi",
+                definition:"is 3.14"
+            },
+            {
+                keyword:"Tao",
+                definition:"is 6.28"
+            },
+            {
+                keyword:"Astrology",
+                definition:"the believe that the stars can determine one's life properties or story"
+            },
+            {
+                keyword:"Astronomy",
+                definition:"is science that study the stars"
+            }
+        ]
+    }
+];
